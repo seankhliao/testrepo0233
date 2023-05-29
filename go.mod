ed114fd479eafbc6ea847282ab3fe59e38877270
Sean Liao <sean+git@liao.dev>
2023-05-29 21:25:53 +0100

    bump

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go.seankhliao.com/testrepo0233
 
 go 1.21
 
-require go.seankhliao.com/testrepo0231 v0.1.0
+require go.seankhliao.com/testrepo0231 v0.2.0
--- right.go
+++ right.go
@@ -2,7 +2,6 @@ package testrepo0232
 
 import (
 	"go.seankhliao.com/testrepo0231"
-	"go.seankhliao.com/testrepo0231/sub"
 )
 
-var Right = testrepo0231.Root + sub.Sub
+var Right = testrepo0231.Root + testrepo0231.Sub
